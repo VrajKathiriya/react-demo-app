13ab9a08c296ebe11151c4b5db8d971727e8bd26
vraj <vrajkathiriya2510@gmail.com>
2023-08-05 19:16:49 +0530

    add from and add validation using formik and yup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import "./index.js";
 import { Route, Routes, NavLink } from "react-router-dom";
 import Welcome from "./components/Welcome";
-import User from "./components/User";
-import { CssBaseline } from "@material-ui/core";
+import Register from "./components/Register";
+import { CssBaseline, useTheme } from "@material-ui/core";
 import Home from "./components/Home";
 import { purple } from "@material-ui/core/colors";
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+// import useTheme from '@material-ui/core/styles';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./components/Login";
 
 const theme = createTheme({
   palette: {
@@ -21,49 +26,69 @@ const theme = createTheme({
       // This is green.A700 as hex.
       main: "#11cb5f",
     },
-    typography: {
-      fontFamily: "Fira Sans",
-      h1: {
-        fontWeight: 300,
-        fontSize: "6rem",
-        lineHeight: 1.167,
-        letterSpacing: "-0.01562em",
-        color: "#ff4081 !important",
-      },
+  },
+  typography: {
+    fontFamily: "Fira Sans",
+    fontSize: 15,
+    h1: {
+      fontWeight: 300,
+      fontSize: "6rem",
+      lineHeight: 1.167,
+      letterSpacing: "-0.01562em",
+      color: "#ff4081 !important",
     },
   },
 });
 
 function App() {
+  // const theme=useTheme();
+
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer />
+
+      <CssBaseline />
+
       <div>
         <div
           style={{
             display: "flex",
             justifyContent: "space-around",
             alignItems: "center",
-            background: "linear-gradient(120deg,#233142,#455d7a,#233142)",
+            background: "linear-gradient(120deg,pink,rgb(193, 132, 250),pink)",
             height: 70,
+            boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.15)",
           }}
         >
-          <NavLink to="/" className="link">
-            HOME
+          <NavLink
+            to="/"
+            style={{ fontSize: "1.2rem", textDecoration: "none" }}
+          >
+            Home🏠
           </NavLink>
-
-          <NavLink to="/welcome" className="link">
-            WELCOME
+          <br />
+          <NavLink
+            to="/welcome"
+            style={{ fontSize: "1.3rem", textDecoration: "none" }}
+          >
+            {" "}
+            Welcome 👋
           </NavLink>
-
-          <NavLink to="/user" className="link">
-            USER
+          <br />
+          <NavLink
+            to="/register"
+            style={{ fontSize: "1.3rem", textDecoration: "none" }}
+          >
+            {" "}
+            Register{" "}
           </NavLink>
         </div>
 
         <Routes path="/">
           <Route index element={<Home />} />
           <Route path="/welcome" element={<Welcome />} />
-          <Route path="/user" element={<User name="Vraj" role="student" />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </ThemeProvider>
